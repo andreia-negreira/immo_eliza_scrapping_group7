7add648ef44e23035127becc272cb25113ff39e8
andreia-negreira <andreia_heringer@hotmail.com>
2023-06-28 14:46:26 +0200

    function save in csv created

--- main_Andreia.py
+++ main_Andreia.py
@@ -3,6 +3,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import json
+import re
+import pandas as pd
 
 root_url = "https://www.immoweb.be/en/search/house/for-sale?countries=BE&priceType=SALE_PRICE&page=1&orderBy=relevance"
 range_links = range(1, 12)
@@ -14,26 +17,29 @@ cookies = req.cookies.get_dict()
 #print(cookies)
 #print(req.status_code)
 
+    
 def get_links():
-    '''Function to find all the links '''
+    """
+    Function that gets links of all houses and appartments
+    """
     s = requests.Session()
-    req = requests.get(root_url)
-    soup = BeautifulSoup(req.text, "html.parser")
-    
-    links = []
-    for elem in soup.find_all("a", attrs={"class": "card__title-link"}):
-       links.append(elem.get("href"))
-    links = links[0:30]
-    
-    new_links = []
-    for num in root_url:
-        if num.isnumeric():
-            num = int(num)
-            while num < 12:
-                num += 1
-                print(f"https://www.immoweb.be/en/search/house/for-sale?countries=BE&priceType=SALE_PRICE&page={num}&orderBy=relevance")
-    
+    listOfLinks = []
+    listOfLinksFinal = []
+    pagesToSearch = 3 # set the number accordint to how many pages you want to sarch thru
 
+    for x in range(1,pagesToSearch+1):
+        search_url = f"https://www.immoweb.be/en/search/house/for-sale?countries=BE&priceType=SALE_PRICE&page={x}&orderBy=relevance"
+        r = s.get(search_url)
+        soup = BeautifulSoup(r.text,"html.parser")
+        for elem in soup.find_all("a", attrs={"class": "card__title-link"}):
+            listOfLinks.append(elem.get('href'))
+        listOfLinks = listOfLinks[0:30]
+        listOfLinksFinal = listOfLinksFinal + listOfLinks
+        listOfLinks.clear()
+
+    return(listOfLinksFinal)
+    
+'''
 def scrapping_general():
     link_immo = "https://www.immoweb.be/en/classified/house/for-sale/libin/6890/10657263"
     r = requests.get(link_immo)
@@ -48,3 +54,36 @@ scrapping_general()
     
 
 
+def test():
+    url_test = "https://www.immoweb.be/en/search-results/house/for-sale?customerIds=3151988&page=1&orderBy=newest"
+    req_test = requests.get(url_test).json()
+    print(req_test)
+
+r = test()
+py_dict = json.load(r)
+print(py_dict)
+'''
+
+def get_data():
+    url ="https://www.immoweb.be/en/classified/house/for-sale/lede/9340/10660142"
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, "html.parser")
+    data = soup.find('div',attrs = {"class":"container-main-content"}).script.text
+    test = re.findall(r"window.classified = ({.*})", data)
+    result = json.loads(test[0]) # Data dictionary
+
+    return result
+
+
+def save():
+        '''This function saves the information acquired from the previous functions and store them in a csv file in the disk.'''
+        data_immo = get_data()
+        dataframe_immo = pd.DataFrame(data_immo)
+        dataset_csv = dataframe_immo.to_csv("./dataset-immo.csv", sep=" ")
+        return dataset_csv
+    
+
+save({'Name': ['Tom', 'Jack', 'nick', 'juli'],
+        'marks': [99, 98, 95, 90]})
+
+    
