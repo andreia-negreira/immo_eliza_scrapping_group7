bf080d2aaee1ab5983a973b5a2913e236846048e
andreia-negreira <andreia_heringer@hotmail.com>
2023-06-28 15:05:17 +0200

    update get_data function and .gitignore created

--- khosro_main.py
+++ khosro_main.py
@@ -1,64 +1,86 @@
 import requests
 from bs4 import BeautifulSoup
-url="https://www.immoweb.be/en/classified/house/for-sale/lede/9340/10660142"
-req= requests.get(url)
-print(req.status_code)
-soup= BeautifulSoup(req.text, "html.parser")
 import json
 import re
+
+url="https://www.immoweb.be/en/classified/house/for-sale/lede/9340/10660142"
+
+req= requests.get(url)
+# print(req.status_code)
+soup= BeautifulSoup(req.text, "html.parser")
 data = soup.find('div',attrs={"class":"container-main-content"}).script.text
-print(data)
-#print(data)
 #.result = soup.find("script", string=lambda t: "window.dataLayer" in t).string # Script element
-test= re.findall(r"window.classified = (\{.*\})", data)
-result = json.loads(test[0]) # Data dictionary 
-print(result)
-# # a=(data[0]['classified'])  
+Raw_data_InList= re.findall(r"window.classified = (\{.*\})", data)
+Raw_data_InDict = json.loads(Raw_data_InList[0]) # Data dictionary
+# print(Raw_data_InDict)
 
-# # # print(data)
-# # data[0]['price']
-# # # for i in a:
-# # #     if i=="price":
-# # #      print(i+1)
-     
-        
-    
-        
-       
-       
-    
-# number_properties = 
-# page_links=[]
+def price():
+    try:
+       property_dict=(Raw_data_InDict["price"])
+       print("Price = ",Raw_data_InDict["price"]['mainValue'])
+    except:
+       print("Price = None")
+price()
 
-# for i in url:
+def living_area():
+    try:
+       print("Living area = ",property_dict['netHabitableSurface'],"m²")
+    except:
+      print("Living area = NA")
+living_area()
 
+def number_of_rooms():
+    try:
+       property_dict=(Raw_data_InDict["property"])
+       print("Number of rooms = ",property_dict['bedroomCount'])
+    except:
+       print("Number of rooms = NA")
+number_of_rooms()
 
+id_dict = Raw_data_InDict["customers"][0]['location']
 
-#     page_links.append
-# links=[]
+def locality():
+   try:
+       print("City = ",id_dict['locality'])
+   except:
+       print("City = NA")
+   try:
+        print("postalCode = ", id_dict['postalCode'])
+   except:
+        print("postalCode = NA")
+   try:
+        a=re.findall("\D.*", id_dict['street'])
+        print("street = ",  a[0])
+   except:
+        print("street = NA")
+   try:
+        if id_dict['number']==None:
+         b=re.findall("\d.*", id_dict['street'])
+         print("number = ",b[0])
+        else:
+         print("number = ", id_dict['number'])
+   except:
+        print("number = NA")
+locality()
 
-# elem=soup.find("a", attrs={"class":"card_title-link"})
-#     # elem=elem.get("href")
-# print(elem)
-#     links.append(elem)
- 
-# #print(links[0:30])
+property_dict=(Raw_data_InDict["property"])
+def kitchen():
+    try:
+       for i in property_dict["kitchen"].keys():
+         if i == 'type':
+           print("kitchen = ",property_dict["kitchen"]["type"])
+    except:
+      print("kitchen = NA")
+kitchen()
 
-    
-
-
-
-# each_properties_url= "https://www.immoweb.be/en/classified/house/for-sale/lede/9340/10660142"     
-# req_each_properties= requests.get(each_properties_url)
-# # print(req_each_properties.status_code)
-# soup= BeautifulSoup(req_each_properties.text, "html.parser")
-# # interior=soup.find("h2", attrs={"class":"text-block__title"})
-# living_area=soup.find_all("td", attrs={"class":"classified-table__data"})[7]
-# import re
-# living_area=re.sub(" " ,"", str(living_area))
-# living_area=re.findall("\d{1,10}",living_area)
-# print(living_area)
-
+def furnished():
+   try:
+       for i in Raw_data_InDict["transaction"]["sale"].keys():
+             if i =='isFurnished':
+               print("Furnished = ",Raw_data_InDict["transaction"]["sale"]["isFurnished"])
+   except:
+       print("Furnished = NA")
+furnished()
 
 
 
