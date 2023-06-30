f16fca6d5f45930092c99be2eec7643bef975134
prngx <sochorm@outlook.com>
2023-06-30 14:46:19 +0200

    updated get_data with Typeerror except

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def get_links(pagesToSearch=1):
     propertiesToSearch = ["apartment","house"] # fill the list with propertis to search
 
     for prop in propertiesToSearch:
-        for x in range(1,pagesToSearch+1): #houses
-            #search_url = f"https://www.immoweb.be/en/search/{prop}/for-sale?countries=BE&priceType=SALE_PRICE&page={x}&orderBy=relevance"
+        for x in range(1,pagesToSearch+1):
             search_url = f"https://www.immoweb.be/en/search/{prop}/for-sale?countries=BE&isNewlyBuilt=false&isAPublicSale=false&page={x}&orderBy=relevance"
             r = s.get(search_url)
             soup = BeautifulSoup(r.text,"html.parser")
@@ -53,35 +52,48 @@ def get_data(url):
         property_dict[url]["Locality"] = Raw_data_InDict["customers"][0]['location']['locality']
     except KeyError:
         property_dict[url]["Locality"] = None
+    except TypeError:
+        property_dict[url]["Locality"] = None
         
     try:
         property_dict[url]["Type of property"] = Raw_data_InDict["property"]["type"]
     except KeyError:
         property_dict[url]["Type of property"] = None
-    
+    except TypeError:
+        property_dict[url]["Type of property"] = None
     try:
         property_dict[url]["Subtype of property"] = Raw_data_InDict["property"]["subtype"]
     except KeyError:
         property_dict[url]["Subtype of property"] = None
-    
+    except TypeError:
+        property_dict[url]["Subtype of property"] = None
+
     try:
         property_dict[url]["Price"] = Raw_data_InDict["price"]["mainValue"]
     except KeyError:
         property_dict[url]["Price"] = None
-        
+    except TypeError:
+        property_dict[url]["Price"] = None
+    
     try:
         property_dict[url]["Type of Transaction"] = Raw_data_InDict["transaction"]["type"]
     except KeyError:
+        property_dict[url]["Type of Transaction"] = None
+    except TypeError:
         property_dict[url]["Type of Transaction"] = None
     
     try:
         property_dict[url]["Nº rooms"] = Raw_data_InDict["property"]["bedroomCount"]
     except KeyError:
-        property_dict[url]["Nº rooms"] = None    
+        property_dict[url]["Nº rooms"] = None
+    except TypeError:    
+        property_dict[url]["Nº rooms"] = None
 
     try:
         property_dict[url]["Living area"] = Raw_data_InDict["netHabitableSurface"]
     except KeyError:
+        property_dict[url]["Living area"] = None
+    except TypeError:
         property_dict[url]["Living area"] = None
 
     try:
@@ -97,11 +109,15 @@ def get_data(url):
             property_dict[url]["Furnished"] = "Yes"
     except KeyError:
         property_dict[url]["Furnished"] = "No"
-    
+    except TypeError:
+        property_dict[url]["Furnished"] = "No"
+
     try:
         if bool(Raw_data_InDict["fireplaceExists"]):
             property_dict[url]["Fireplace"] = "Yes"
     except KeyError:
+        property_dict[url]["Fireplace"] = "No"
+    except TypeError:
         property_dict[url]["Fireplace"] = "No"
     
     try:
@@ -110,14 +126,18 @@ def get_data(url):
             property_dict[url]["Area Terrace"] = Raw_data_InDict["property"]["terraceSurface"]
     except KeyError:
         property_dict[url]["Terrace"] = "No"
-    
+    except TypeError:
+        property_dict[url]["Terrace"] = "No"
+
     try:
         if bool(Raw_data_InDict["property"]["hasGarden"]):
             property_dict[url]["Garden"] = "Yes"
             property_dict[url]["Area Garden"] = Raw_data_InDict["property"]["gardenSurface"]
     except KeyError:
         property_dict[url]["Garden"] = "No"
-    
+    except TypeError:
+        property_dict[url]["Garden"] = "No"
+
     try:
         property_dict[url]["Land Surface"] = Raw_data_InDict["property"]["land"]["surface"]
     except KeyError:
@@ -131,16 +151,22 @@ def get_data(url):
         property_dict[url]["Nº facades"] = Raw_data_InDict["property"]["building"]["facadeCount"]
     except KeyError:
         property_dict[url]["Nº facades"] = None
+    except TypeError:
+        property_dict[url]["Nº facades"] = None
     
     try:
         if bool(Raw_data_InDict["fhasSwimmingPool"]):
             property_dict[url]["Swimming Pool"] = "Yes"
     except KeyError:
         property_dict[url]["Swimming Pool"] = "No"
+    except TypeError:
+        property_dict[url]["Swimming Pool"] = "No"
         
     try:
         property_dict[url]["State Building"] = Raw_data_InDict["property"]["building"]["condition"]
     except KeyError:
+        property_dict[url]["State Building"] = None
+    except TypeError:
         property_dict[url]["State Building"] = None
             
     return property_dict
@@ -155,7 +181,7 @@ def save(new_data):
     return writer
 
 start = time.time()
-links = get_links(1)
+links = get_links(84)
 end = time.time()
 print("Gathering links time: {:.6f}s".format(end-start))
 
@@ -163,7 +189,6 @@ start = time.time()
 with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
     futures = [executor.submit(get_data, link) for link in links]
     for future in concurrent.futures.as_completed(futures):
-        #print(future.result())
         save(future.result())        
 end = time.time()
 print("Time taken for gathering data from {} links: {:.6f}s".format(len(links),end-start))
