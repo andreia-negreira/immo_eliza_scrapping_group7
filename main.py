86a60bcc040fb9457c61932c7722faac62089eb3
prongx <46458979+prongx@users.noreply.github.com>
2023-07-06 14:42:56 +0200

    Updated get_links function. Cleaned code, added comments.

--- main.py
+++ main.py
@@ -4,191 +4,214 @@ import json
 import re
 import pandas as pd
 import concurrent
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor, as_completed
 import time
-import csv
+# import csv
+
 
 def get_links(pagesToSearch=1):
     """
-    Function that gets links of all houses
+    Function that gets links of all properties set in propertiesToSearch list variable. 
+    Parameter pagesToSearch: Set the number according to how many pages you want to search thru each of properties set in functions propertiesToSearch variable.
+    Variable propertiesToSearch(list): Set according to what properties types you want to look for. 
     """
+
+    propertiesToSearch = ["apartment","house"]
     s = requests.Session()
     listOfLinks = []
     listOfLinksFinal = []
-    #pagesToSearch = 1 # set the number according to how many pages you want to sarch thru each of properties set
-    propertiesToSearch = ["apartment","house"] # fill the list with propertis to search
 
     for prop in propertiesToSearch:
-        for x in range(1,pagesToSearch+1):
+        for x in range(1, pagesToSearch+1):
             search_url = f"https://www.immoweb.be/en/search/{prop}/for-sale?countries=BE&isNewlyBuilt=false&isAPublicSale=false&page={x}&orderBy=relevance"
             r = s.get(search_url)
             soup = BeautifulSoup(r.text,"html.parser")
             for elem in soup.find_all("a", attrs = {"class": "card__title-link"}):
                 listOfLinks.append(elem.get('href'))
-            listOfLinks = listOfLinks[0:len(listOfLinks)-30]
-            listOfLinksFinal = listOfLinksFinal + listOfLinks
+            listOfLinks = listOfLinks[0:len(listOfLinks)-30] # 30 last links on each page are "recommended properties", throwing those out. 
+            listOfLinksFinal = listOfLinksFinal + listOfLinks # Appending the first 30 properties to the output list.
             listOfLinks.clear()
     return(listOfLinksFinal)
 
-"""with open('links.txt','w+') as file:
-    file.write('\n'.join(links))"""
-
 def get_data(url):
-    #url="https://www.immoweb.be/en/classified/house/for-sale/lede/9340/10660142"
     s = requests.Session()
     req= s.get(url)
+    if req.status_code == 200: # Checking if the link is still valid
 
-    soup= BeautifulSoup(req.text, "html.parser")
-    data = soup.find('div',attrs={"class":"container-main-content"}).script.text
-
-    Raw_data_InList= re.findall(r"window.classified = (\{.*\})", data)
-    Raw_data_InDict = json.loads(Raw_data_InList[0]) # Data dictionary
-
-
-    property_dict = {}
-    property_dict[url] = {}
-    
-    try:
-        property_dict[url]["Locality"] = Raw_data_InDict["customers"][0]['location']['locality']
-    except KeyError:
-        property_dict[url]["Locality"] = None
-    except TypeError:
-        property_dict[url]["Locality"] = None
+        soup = BeautifulSoup(req.text, "html.parser")
+        data = soup.find('div',attrs={"class":"container-main-content"}).script.text
+        Raw_data_InList= re.findall(r"window.classified = (\{.*\})", data)
+        Raw_data_InDict = json.loads(Raw_data_InList[0]) # Data dictionary
+        property_dict = {"url":url}
         
-    try:
-        property_dict[url]["Type of property"] = Raw_data_InDict["property"]["type"]
-    except KeyError:
-        property_dict[url]["Type of property"] = None
-    except TypeError:
-        property_dict[url]["Type of property"] = None
-    try:
-        property_dict[url]["Subtype of property"] = Raw_data_InDict["property"]["subtype"]
-    except KeyError:
-        property_dict[url]["Subtype of property"] = None
-    except TypeError:
-        property_dict[url]["Subtype of property"] = None
+        try:
+            property_dict["locality"] = Raw_data_InDict["property"]['location']['locality']
+        except KeyError:
+            property_dict["locality"] = 0
+        except TypeError:
+            property_dict["locality"] = 0
 
-    try:
-        property_dict[url]["Price"] = Raw_data_InDict["price"]["mainValue"]
-    except KeyError:
-        property_dict[url]["Price"] = None
-    except TypeError:
-        property_dict[url]["Price"] = None
-    
-    try:
-        property_dict[url]["Type of Transaction"] = Raw_data_InDict["transaction"]["type"]
-    except KeyError:
-        property_dict[url]["Type of Transaction"] = None
-    except TypeError:
-        property_dict[url]["Type of Transaction"] = None
-    
-    try:
-        property_dict[url]["Nº rooms"] = Raw_data_InDict["property"]["bedroomCount"]
-    except KeyError:
-        property_dict[url]["Nº rooms"] = None
-    except TypeError:    
-        property_dict[url]["Nº rooms"] = None
-
-    try:
-        property_dict[url]["Living area"] = Raw_data_InDict["netHabitableSurface"]
-    except KeyError:
-        property_dict[url]["Living area"] = None
-    except TypeError:
-        property_dict[url]["Living area"] = None
-
-    try:
-        if bool(Raw_data_InDict["property"]["kitchen"]['type']):
-            property_dict[url]["Equipped kitchen"] = "Yes"
-    except KeyError:
-        property_dict[url]["Equipped kitchen"] = "No"
-    except TypeError: #added this except for NoneType' object is not subscriptable
-        property_dict[url]["Equipped kitchen"] = "No"
-
-    try:
-        if bool(Raw_data_InDict["property"]["transaction"]["sale"]["isFurnished"]):
-            property_dict[url]["Furnished"] = "Yes"
-    except KeyError:
-        property_dict[url]["Furnished"] = "No"
-    except TypeError:
-        property_dict[url]["Furnished"] = "No"
-
-    try:
-        if bool(Raw_data_InDict["fireplaceExists"]):
-            property_dict[url]["Fireplace"] = "Yes"
-    except KeyError:
-        property_dict[url]["Fireplace"] = "No"
-    except TypeError:
-        property_dict[url]["Fireplace"] = "No"
-    
-    try:
-        if bool(Raw_data_InDict["property"]["hasTerrace"]):
-            property_dict[url]["Terrace"] = "Yes"
-            property_dict[url]["Area Terrace"] = Raw_data_InDict["property"]["terraceSurface"]
-    except KeyError:
-        property_dict[url]["Terrace"] = "No"
-    except TypeError:
-        property_dict[url]["Terrace"] = "No"
-
-    try:
-        if bool(Raw_data_InDict["property"]["hasGarden"]):
-            property_dict[url]["Garden"] = "Yes"
-            property_dict[url]["Area Garden"] = Raw_data_InDict["property"]["gardenSurface"]
-    except KeyError:
-        property_dict[url]["Garden"] = "No"
-    except TypeError:
-        property_dict[url]["Garden"] = "No"
-
-    try:
-        property_dict[url]["Land Surface"] = Raw_data_InDict["property"]["land"]["surface"]
-    except KeyError:
-        property_dict[url]["Land Surface"] = None
-    except TypeError: #added this except for NoneType' object is not subscriptable
-        property_dict[url]["Land Surface"] = None
-    
-    # surface area of the plot of land
-    
-    try:
-        property_dict[url]["Nº facades"] = Raw_data_InDict["property"]["building"]["facadeCount"]
-    except KeyError:
-        property_dict[url]["Nº facades"] = None
-    except TypeError:
-        property_dict[url]["Nº facades"] = None
-    
-    try:
-        if bool(Raw_data_InDict["fhasSwimmingPool"]):
-            property_dict[url]["Swimming Pool"] = "Yes"
-    except KeyError:
-        property_dict[url]["Swimming Pool"] = "No"
-    except TypeError:
-        property_dict[url]["Swimming Pool"] = "No"
-        
-    try:
-        property_dict[url]["State Building"] = Raw_data_InDict["property"]["building"]["condition"]
-    except KeyError:
-        property_dict[url]["State Building"] = None
-    except TypeError:
-        property_dict[url]["State Building"] = None
+        try:
+            property_dict["postal_code"] = Raw_data_InDict["property"]['location']['postalCode']
+        except KeyError:
+            property_dict["postal_code"] = 0
+        except TypeError:
+            property_dict["postal_code"] = 0    
             
+        try:
+            property_dict["type_property"] = Raw_data_InDict["property"]["type"]
+        except KeyError:
+            property_dict["type_property"] = 0
+        except TypeError:
+            property_dict["type_property"] = 0
+        try:
+            property_dict["subtype_property"] = Raw_data_InDict["property"]["subtype"]
+        except KeyError:
+            property_dict["subtype_property"] = 0
+        except TypeError:
+            property_dict["subtype_property"] = 0
+
+        try:
+            property_dict["price"] = Raw_data_InDict["price"]["mainValue"]
+        except KeyError:
+            property_dict["price"] = 0
+        except TypeError:
+            property_dict["price"] = 0
+        
+        try:
+            property_dict["type-transaction"] = Raw_data_InDict["transaction"]["type"]
+        except KeyError:
+            property_dict["type-transaction"] = 0
+        except TypeError:
+            property_dict["type-transaction"] = 0
+        
+        try:
+            property_dict["n_rooms"] = Raw_data_InDict["property"]["bedroomCount"]
+        except KeyError:
+            property_dict["n_rooms"] = 0
+        except TypeError:    
+            property_dict["n_rooms"] = 0
+
+        try:
+            property_dict["living_area"] = Raw_data_InDict["property"]["netHabitableSurface"]
+        except KeyError:
+            property_dict["living_area"] = 0
+        except TypeError:
+            property_dict["living_area"] = 0
+
+        try:
+            if bool(Raw_data_InDict["property"]["kitchen"]['type']):
+                property_dict["equipped_kitchen"] = 1
+            else:
+                property_dict["equipped_kitchen"] = 0
+        except KeyError:
+            property_dict["equipped_kitchen"] = 0
+        except TypeError: 
+            property_dict["equipped_kitchen"] = 0
+
+        try:
+            if bool(Raw_data_InDict["transaction"]["sale"]["isFurnished"]):
+                property_dict["furnished"] = 1
+            else:
+                property_dict["furnished"] = 0
+        except KeyError:
+            property_dict["furnished"] = 0
+        except TypeError:
+            property_dict["furnished"] = 0
+
+        try:
+            if bool(Raw_data_InDict["property"]["fireplaceExists"]):
+                property_dict["fireplace"] = 1
+            else:
+                property_dict["fireplace"] = 0
+        except KeyError:
+            property_dict["fireplace"] = 0
+        except TypeError:
+            property_dict["fireplace"] = 0
+        
+        try:
+            if bool(Raw_data_InDict["property"]["hasTerrace"]):
+                property_dict["terrace"] = 1
+                property_dict["area_terrace"] = Raw_data_InDict["property"]["terraceSurface"]
+            else:
+                property_dict["terrace"] = 0
+                property_dict["area_terrace"] = 0
+        except KeyError:
+            property_dict["terrace"] = 0
+        except TypeError:
+            property_dict["terrace"] = 0
+
+        try:
+            if bool(Raw_data_InDict["property"]["hasGarden"]):
+                property_dict["garden"] = 1
+                property_dict["area-garden"] = Raw_data_InDict["property"]["gardenSurface"]
+            else:
+                property_dict["garden"] = 0
+                property_dict["area-garden"] = 0 
+        except KeyError:
+            property_dict["garden"] = 0
+        except TypeError:
+            property_dict["garden"] = 0
+
+        try:
+            if Raw_data_InDict["property"]["land"]["surface"] != None:
+                property_dict["land-surface"] = Raw_data_InDict["property"]["land"]["surface"]
+            else:
+                property_dict["land-surface"] = 0
+        except KeyError:
+            property_dict["land-surface"] = 0
+        except TypeError: 
+            property_dict["land-surface"] = 0
+        
+        try:
+            property_dict["n-facades"] = Raw_data_InDict["property"]["building"]["facadeCount"]
+        except KeyError:
+            property_dict["n-facades"] = 0
+        except TypeError:
+            property_dict["n-facades"] = 0
+        
+        try:
+            if bool(Raw_data_InDict["property"]["hasSwimmingPool"]):
+                property_dict["swimming-pool"] = 1
+            else:
+                property_dict["swimming-pool"] = 0
+        except KeyError:
+            property_dict["swimming-pool"] = 0
+        except TypeError:
+            property_dict["swimming-pool"] = 0
+            
+        try:
+            property_dict["state-building"] = Raw_data_InDict["property"]["building"]["condition"]
+        except KeyError:
+            property_dict["state-building"] = 0
+        except TypeError:
+            property_dict["state-building"] = 0
+
+    else: # If the link is not valid, pass just the link url into the dictionary
+        property_dict = {"url":url}
+
     return property_dict
 
-data_immo = {}
+data_immo = []
 def save(new_data):
-    '''This function saves the information acquired from the previous functions and store them in a csv file in the disk.'''
-    with open("./dataset-immo.csv", 'a', newline='', encoding='utf-8') as f:
-        writer = csv.DictWriter(f, fieldnames=new_data.keys())
-        writer.writeheader()
-        writer.writerow(new_data)
-    return writer
+        '''This function saves the information acquired from the previous functions and store them in a csv file in the disk.'''
+        data_immo.append(new_data)
+        dataframe_immo = pd.DataFrame(data_immo)
+        dataframe_immo.to_csv("./dataset-immo.csv", index=False, encoding="utf-8")
+        print(dataframe_immo)
+        return dataframe_immo
 
-start = time.time()
-links = get_links(167)
+start = time.time() # Scraping links for later usage.
+links = get_links(1) # Set the number according to how many pages you want to search thru each of properties set in get_links function's propertiesToSearch variable.
 end = time.time()
 print("Gathering links time: {:.6f}s".format(end-start))
-"""
-start = time.time()
-with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+
+# with open('links.txt', 'r') as f:
+#     links = [line.strip() for line in f]
+
+start = time.time() # Using concurrency to spead up the scraping process. 
+with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor: 
     futures = [executor.submit(get_data, link) for link in links]
     for future in concurrent.futures.as_completed(futures):
         save(future.result())        
 end = time.time()
-print("Time taken for gathering data from {} links: {:.6f}s".format(len(links),end-start))"""
+print("Time taken for gathering data from {} links: {:.6f}s".format(len(links),end-start))
